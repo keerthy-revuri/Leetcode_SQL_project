ef61fb2e848ed1d17f4806377690e1c265d3421d
keerthy-revuri <156633346+keerthy-revuri@users.noreply.github.com>
2024-07-08 16:05:59 -0400

    Update Consecutive available seats.sql

--- Easy/Consecutive available seats.sql
+++ Easy/Consecutive available seats.sql
@@ -31,3 +31,23 @@ lag(free,1) over() as prev
 from cinema) a
 where a.free=True and (next = True or prev=True)
 order by seat_id
+
+
+#logic 2 
+ With t1 as (
+Select * , row_number() over ( order by seat_id) as rw,
+ (seat_id - row_number() over ( order by seat_id) ) as grp from cinema where free = 1)
+select seat_id from(select * , count(*) over (partition by grp) as cnt from t1) where cnt > 1
+
+#logic 3
+#using inner join
+ 
+select distinct c1.seat_id
+from cinema c1 inner join cinema c2
+on (c1.seat_id+1=c2.seat_id or c1.seat_id-1=c2.seat_id)
+where c1.free=1 
+and c2.free=1
+order by c1.seat_id
+;
+
+
